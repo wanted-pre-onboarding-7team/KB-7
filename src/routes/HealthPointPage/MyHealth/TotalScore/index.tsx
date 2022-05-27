48bc0c8d0103ca8b8f96f63a91e71639cac95cfe
silent10z <dkgueing@gmail.com>
2022-05-27 14:39:00 +0900

    feat: pieChart 가운데 점수 변경

--- src/routes/HealthPointPage/MyHealth/TotalScore/index.tsx
+++ src/routes/HealthPointPage/MyHealth/TotalScore/index.tsx
@@ -1,12 +1,12 @@
-import { VictoryAnimation, VictoryLabel, VictoryPie } from 'victory'
+import { VictoryPie } from 'victory'
 import dayjs from 'dayjs'
 
 import healthInfo from 'assets/data/healthInfo.json'
 
-import CustomLabel from './CustomLabel'
 import CustomSlice from './CustomSlice'
 
 import styles from './totalScore.module.scss'
+import { IIcon } from 'assets/svgs'
 
 const TotalScore = () => {
   const healthScore = Number(healthInfo.userInfo.healthScore)
@@ -16,18 +16,25 @@ const TotalScore = () => {
 
   return (
     <div className={styles.totalScore}>
-      <h2 className={styles.title}>김헬스님의 건강 점수</h2>
+      <h2 className={styles.title}>
+        김헬스님의 건강점수
+        <IIcon className={styles.iIcon} />
+      </h2>
+
       <div>
         <VictoryPie
           data={data}
           cornerRadius={50}
           startAngle={-135}
           endAngle={135}
+          labels={() => null}
           innerRadius={120}
           dataComponent={<CustomSlice />}
-          labelComponent={<CustomLabel healthScore={healthScore} />}
           animate={{ duration: 1000 }}
         />
+        <div className={styles.pieChartText}>
+          <mark>{healthScore}</mark>점
+        </div>
       </div>
       <div className={styles.sub}>
         <p className={styles.healthDate}>{healthDate}</p>
